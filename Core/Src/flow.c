6a5c381267558fa66dba8cb8567325714497d2bd
Andrei Trif <andrei3f@yahoo.com>
2025-07-24 15:33:01 +0300

    code refactor

--- Core/Src/flow.c
+++ Core/Src/flow.c
@@ -16,16 +16,16 @@
 
 static state_t state = WAITING_OPTION;
 
-static uint32_t task_options[2] = {
-   [BUTTON_INTERVAL_RECORDING_TIME] = 10000,
-   [BUTTON_INTERVAL_KEEP_LED_ON_TIME] = 1000,
-};
+static uint32_t task_options[2] = { [BUTTON_INTERVAL_RECORDING_TIME] = 10000,
+                                    [BUTTON_INTERVAL_KEEP_LED_ON_TIME] = 1000, };
 
-void set_option(option_t opt, uint32_t value) {
+void set_option(option_t opt, uint32_t value)
+{
     task_options[opt] = value;
 }
 
-uint32_t get_option(option_t opt) {
+uint32_t get_option(option_t opt)
+{
     return task_options[opt];
 }
 
@@ -47,34 +47,34 @@ void init_option(state_t s)
         case TIMER_TOGGLE:
         {
             HAL_CHECK(HAL_TIM_Base_Start_IT(&htim4));
-        }
             break;
+        }
         case ADC_READING:
         {
             HAL_CHECK(HAL_TIM_OC_Start_IT(&htim1, TIM_CHANNEL_1));
             HAL_CHECK(HAL_ADC_Start_IT(&hadc3));
-        }
             break;
+        }
         case ADC_LED_TOGGLE:
         {
             HAL_CHECK(HAL_TIM_OC_Start_IT(&htim1, TIM_CHANNEL_1));
             HAL_CHECK(HAL_TIM_Base_Start_IT(&htim2));
             HAL_CHECK(HAL_ADC_Start_IT(&hadc3));
-        }
             break;
+        }
         case ADC_LED_TOGGLE_PWM:
         {
             HAL_CHECK(HAL_TIM_OC_Start_IT(&htim1, TIM_CHANNEL_1));
             HAL_CHECK(HAL_ADC_Start_IT(&hadc3));
             HAL_CHECK(HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3));
-        }
             break;
+        }
         case BUTTON_INTERVAL:
         {
             htim9.Instance->ARR = get_option(BUTTON_INTERVAL_RECORDING_TIME);
             HAL_CHECK(HAL_TIM_Base_Start_IT(&htim9));
-        }
             break;
+        }
         default:
             break;
     }
@@ -97,28 +97,28 @@ void deinit_options(void)
         case TIMER_TOGGLE:
         {
             HAL_CHECK(HAL_TIM_Base_Stop_IT(&htim4));
-        }
             break;
+        }
         case ADC_READING:
         {
             HAL_CHECK(HAL_TIM_OC_Stop_IT(&htim1, TIM_CHANNEL_1));
             HAL_CHECK(HAL_ADC_Stop_IT(&hadc3));
-        }
             break;
+        }
         case ADC_LED_TOGGLE:
         {
             HAL_CHECK(HAL_TIM_OC_Stop_IT(&htim1, TIM_CHANNEL_1));
             HAL_CHECK(HAL_TIM_Base_Stop_IT(&htim2));
             HAL_CHECK(HAL_ADC_Stop_IT(&hadc3));
-        }
             break;
+        }
         case ADC_LED_TOGGLE_PWM:
         {
             HAL_CHECK(HAL_TIM_OC_Stop_IT(&htim1, TIM_CHANNEL_1));
             HAL_CHECK(HAL_ADC_Stop_IT(&hadc3));
             HAL_CHECK(HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_3));
-        }
             break;
+        }
         case BUTTON_INTERVAL:
         {
             reset_recordings();
@@ -126,8 +126,8 @@ void deinit_options(void)
 
             // We might be in the recording phase so TIM6 might not have started yet.
             HAL_TIM_Base_Stop_IT(&htim6);
-        }
             break;
+        }
         default:
             break;
     }
