1353b62192a74a2774143a216d16c8e2db5dfc79
Andrei Trif <andrei3f@yahoo.com>
2025-09-17 10:31:53 +0300

    switch to fixed point arithmetic + use stdint for numbers

--- Core/Inc/graph.h
+++ Core/Inc/graph.h
@@ -8,15 +8,17 @@
 #ifndef INC_GRAPH_H_
 #define INC_GRAPH_H_
 
+#include <stdint.h>
+
 typedef enum
 {
     MODE_BAR,
     MODE_POINT,
 } graph_display_mode_t;
 
-void graph_init(int x_samples, int y_samples, char *x_axis_title, char *y_axis_title, char *title);
+void graph_init(uint32_t x_samples, uint32_t y_samples, char *x_axis_title, char *y_axis_title, char *title);
 void graph_draw_axis(void);
-void graph_update_x_value(int x, int y);
+void graph_update_x_value(uint32_t x, uint32_t y);
 void graph_change_mode(graph_display_mode_t display_mode);
 
 #endif /* INC_GRAPH_H_ */
--- Core/Src/graph.c
+++ Core/Src/graph.c
@@ -14,12 +14,14 @@
 #define TEXT_8BIT_HEIGHT 8
 #define TEXT_8BIT_WIDTH 4
 
-static int x_size = 0;
-static int y_size = 0;
-static int x_gap = 10;
-static int y_gap = 20;
-static int pixels_per_sample = 0;
-static float y_pixels_per_sample = 0;
+#define FIXED_POINT_SHIFT 16
+
+static uint32_t x_size = 0;
+static uint32_t y_size = 0;
+static uint32_t x_gap = 10;
+static uint32_t y_gap = 20;
+static uint32_t pixels_per_sample = 0;
+static uint32_t y_pixels_per_sample = 0;
 
 static char x_title[40];
 static char y_title[40];
@@ -35,7 +37,7 @@ static uint32_t line_color[6] = { LCD_COLOR_GREEN, LCD_COLOR_ORANGE, LCD_COLOR_R
                                   LCD_COLOR_RED,   LCD_COLOR_RED,    LCD_COLOR_RED };
 static int intensity_div = 0;
 
-void graph_init(int x_samples, int y_samples, char *x_axis_title, char *y_axis_title, char *title)
+void graph_init(uint32_t x_samples, uint32_t y_samples, char *x_axis_title, char *y_axis_title, char *title)
 {
     x_size = x_samples;
     y_size = y_samples;
@@ -46,7 +48,7 @@ void graph_init(int x_samples, int y_samples, char *x_axis_title, char *y_axis_t
     pixels_per_sample = (w - TEXT_8BIT_WIDTH * 2) / x_samples;
     x_gap = (w - pixels_per_sample * x_samples) / 2;
 
-    y_pixels_per_sample = (float)(h - y_gap * 2) / y_samples;
+    y_pixels_per_sample = ((h - y_gap * 2) << FIXED_POINT_SHIFT) / y_samples;
 
     intensity_div = y_samples / (sizeof(line_color) / sizeof(*line_color));
 
@@ -80,7 +82,7 @@ void graph_draw_axis(void)
 
     // Vertical Text
     int height = (TEXT_8BIT_HEIGHT + 1) * strlen(y_title);
-    for (int i = 0; i < strlen(y_title); ++i)
+    for (size_t i = 0; i < strlen(y_title); ++i)
     {
         BSP_LCD_DisplayChar(x_gap / 2, h / 2 - height / 2 + i * (TEXT_8BIT_HEIGHT + 1), y_title[i]);
     }
@@ -100,10 +102,10 @@ void graph_change_mode(graph_display_mode_t display_mode)
     mode = display_mode;
 }
 
-void graph_update_x_value(int x, int y)
+void graph_update_x_value(uint32_t x, uint32_t y)
 {
-    int screen_x = x_gap + x * pixels_per_sample;
-    int screen_y = h - (int)((float)y * y_pixels_per_sample) - y_gap;
+    uint32_t screen_x = x_gap + x * pixels_per_sample;
+    uint32_t screen_y = h - ((y * y_pixels_per_sample) >> FIXED_POINT_SHIFT) - y_gap;
 
     BSP_LCD_SetTextColor(bg_color);
     switch (mode)
